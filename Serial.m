5076ba6b58bc35a8f4727c506a56a207d1064c03
Valeriu Balaban <balaban.valeriu90@gmail.com>
2016-05-22 20:41:55 +0200

    Adds documentation related to serial protocol

--- Serial.m
+++ Serial.m
@@ -1,6 +1,27 @@
 classdef Serial < handle
-    %SERIAL Summary of this class goes here
-    %   Detailed explanation goes here
+    % SERIAL Implements a protocol for communication with Glider
+    % 
+    % All packets are ASCII encoded and ended by a new line, \n - ASCII 13
+    % List of packets sent by Glider and acknowledged by Ground Station:
+    %
+    %   + Sensors Packet
+    %       SENT 8099, Mission Time, Packet Count, ..., Command Count \n
+    %       RECV ACK-SENSOR, Packet Count \n
+    % 
+    %   + Image Packet
+    %       SENT IMAGE, Frame Number, Image Data as HEX string \n
+    %       RECV ACK-IMAGE, Frame Number \n
+    % 
+    %   + Log Packet
+    %       SENT LOG, Log Number, Message String \n
+    %       RECV ACK-LOG, Log Number \n
+    % 
+    %
+    % List of packets sent by Ground Station and acknowledged by Glider:
+    % 
+    %   + Command Packet
+    %       SENT COMMAND, Command Number, Command Name, Args, ... \n
+    %       RECV ACK-COMMAND, Command Number \n
     
     properties(Hidden = true)
         serialPort;
